71591a228b1d01d960d3adf71551ed171ad41bb8
Eshan Jha <eshan-jha@outlook.com>
2025-10-29 21:11:40 -0700

    modify Playground

--- JavaProjects/Playground.java
+++ JavaProjects/Playground.java
@@ -4,11 +4,20 @@ public class Playground {
             return n;
         }
         else {
-            return n * factorial(n - 1);
+            return n - factorial(n - 1);
+        }
+    }
+
+    public static int fibbanoci(int n) {
+        if (n == 0) {
+            return n;
+        }
+        else {
+            return fibbanoci(n - 1) + fibbanoci(n - 2);
         }
     }
     public static void main(String[] args) {
-        int result = factorial(5);
+        int result = fibbanoci(5);
         System.out.println(result);
     } 
 }
