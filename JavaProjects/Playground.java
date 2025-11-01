f771da139e025fffd107858efae4ae68eed08837
Eshan Jha <eshan-jha@outlook.com>
2025-11-01 02:58:27 -0700

    modify Playground and create new file called PlaygroundAccessor, used to test Playground code

--- JavaProjects/Playground.java
+++ JavaProjects/Playground.java
@@ -1,10 +1,22 @@
-public class Playground {
-    int X = 5;
-    public static void main(String[] args) {
-        Playground playground1 = new Playground();
-        Playground playground2 = new Playground();
-        playground2.X = 9;
-        System.out.println(playground1.X);
-        System.out.println(playground2.X);
+public  class Playground {
+    private int age = 13;
+
+    public int getAge() {
+        return age;
     }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+    
+
+
+    
+    
+    
+
+    
+
+    
+    
 }
