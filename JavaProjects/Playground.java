be6b80641dc6d7e24ec5ff3e2115d2a1a305ecb6
Eshan Jha <eshan-jha@outlook.com>
2025-11-03 04:08:38 -0800

    modify Playground and data.txt

--- JavaProjects/Playground.java
+++ JavaProjects/Playground.java
@@ -1,18 +1,20 @@
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 public  class Playground {
     public static void main(String[] args) {
         try {
-            FileWriter writer = new FileWriter("data.txt", true);
-            writer.write("username: Eshan/n");
-            writer.write("password: hello123");
-            writer.close();
-            System.out.println("Data Saved Successfully");
+            BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
         }
         catch (IOException e) {
-            System.out.println("Error!");
-            e.printStackTrace();
+          System.out.println("Error!");
+          e.printStackTrace();
         }
     }
     
