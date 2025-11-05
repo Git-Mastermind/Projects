34b4649d3b2673bf38bad5e5b0f7ca7dd046a7a7
Eshan Jha <eshan-jha@outlook.com>
2025-11-05 00:22:05 -0800

    modify User and UserOptions

--- User/User.java
+++ User/User.java
@@ -1,19 +1,89 @@
 package User;
 import java.util.Scanner;
 import java.lang.System;
+import java.util.ArrayList;
+
+
 
 public class User {
-    UserOptions user = new UserOptions();
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserOptions user = new UserOptions();
+        Time time = new Time();
+        boolean loggedIn = false;
+        while (true) {
+            System.out.println(user.titleScreen());
+            System.out.print("Choose an option: ");
+            int optionInput = scanner.nextInt();
+            scanner.nextLine();
 
-    System.out.print("Username: ");
-    String username = scanner.nextLine();
+            if (optionInput == 1) {
+                int authorizationCode = user.logIn();
+                if (authorizationCode == 1) {
+                    loggedIn = true;
+                }
+                else {
+                    System.out.println("❌ Username or password is incorrect");
+                }
+            }
 
-    System.out.print("Password: ")
-    String password = scanner.nextLine();
+            else if (optionInput == 2) {
+                ArrayList<String> credentials = user.signUpInputs();
+                String username = credentials.getFirst();
+                String password = credentials.get(1);
+
+                int authorizationCode = user.signUp(username, password);
+                System.out.println("Authorizing...");
+                time.sleep(2000);
+                if (authorizationCode == 1) {
+                    System.out.println("Successfully signed up!");
+                    time.sleep(2000);
+                }
+                else {
+                    System.out.println("ERROR ENCOUNTERED!");
+                    time.sleep(1000);
+                }
+            }
+            if (optionInput == 3) {
+                String username = user.returnUsername();
+                String password = user.forgotPassword(username);
+                System.out.println("Authorizing...");
+                time.sleep(2000);
+                System.out.println("Opening file...");
+                time.sleep(500);
+                System.out.println("Double checking...");
+                time.sleep(750);
+                System.out.println("Encrypting...");
+                time.sleep(750);
+                System.out.println("Retreiving...");
+                time.sleep(300);
+
+                if (password.equals("Xy9Pq7Vt_L3n8HrD2mZ5bFsKjT6wNcA1")) {
+                    System.out.println("❌ Username not found");
+                }
+                else {
+                    System.out.println("🔑 Your password is: " + password + "!");
+                    time.sleep(2000);
+                }
+            }
+            if (optionInput == 101) {
+                System.out.println("Current working directory: " + System.getProperty("user.dir"));
+            }
+
+            }
+
+            }
 
 
 
 
 
-}
+
+
+
+
+
+    }
+
+
+
