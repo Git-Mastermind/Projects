6f3dadcb4122479d74f37f8c0561dd5243c8e86c
Eshan Jha <eshan-jha@outlook.com>
2026-01-02 10:52:38 -0800

    modify Games, modify Main

--- Java-Projects/Games.java
+++ Java-Projects/Games.java
@@ -16,4 +16,35 @@ public class Games {
             System.out.println(number);
         scanner.close();
     }
+
+    public void numberGuesser() {
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 101);
+        int numberOfGuesses = 7;
+        while (true) {
+            System.out.print("Guess (" + numberOfGuesses + "): ");
+            int numberGuess = scanner.nextInt();
+
+            if (numberOfGuesses == 1) {
+                System.out.println("You ran out of guesses! The number was " + randomNumber);
+                break;
+            }
+            else if (numberGuess > 100 || numberGuess < 1) {
+                System.out.println("Invalid domain (1 - 100)!");
+            }
+            else if (numberGuess == randomNumber) {
+                System.out.println("You win! The number was " + randomNumber);
+                break;
+            }
+            else if (numberGuess > randomNumber) {
+                System.out.println("Too high!");
+                numberOfGuesses--;
+            }
+            else if (numberGuess < randomNumber) {
+                System.out.println("Too low!");
+                numberOfGuesses--;
+            }
+        }
+        scanner.close();
+    }
 }
--- Java-Projects/Main.java
+++ Java-Projects/Main.java
@@ -4,42 +4,15 @@ import java.awt.*;
 import java.nio.channels.InterruptedByTimeoutException;
 import java.text.NumberFormat;
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int randomNumber = (int) (Math.random() * 101);
-        int numberOfGuesses = 7;
-        while (true) {
-            System.out.print("Guess (" + numberOfGuesses + "): ");
-            int numberGuess = scanner.nextInt();
-
-            if (numberOfGuesses == 1) {
-                System.out.println("You ran out of guesses! The number was " + randomNumber);
-                break;
-            }
-            else if (numberGuess > 100 || numberGuess < 1) {
-                System.out.println("Invalid domain (1 - 100)!");
-            }
-
-            else if (numberGuess == randomNumber) {
-                System.out.println("You win! The number was " + randomNumber);
-                break;
-            }
-            else if (numberGuess > randomNumber) {
-                System.out.println("Too high!");
-                numberOfGuesses--;
-            }
-            else if (numberGuess < randomNumber) {
-                System.out.println("Too low!");
-                numberOfGuesses--;
-            }
-        }
-        scanner.close();
         
-
 }
 }
         
